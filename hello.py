a98d3f5cd385cebe1fa640e55dd390f1443bb8ca
Lavanya Mehndiratta <lavanyamehndiratta@Lavanyas-Laptop.local>
2024-09-23 20:16:34 -0400

    Made a form with name and email fields and flash messages

--- hello.py
+++ hello.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap 
 from flask_moment import Moment
 from datetime import datetime, timezone
 from flask_wtf import FlaskForm
+import email_validator
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_application_key_is_difficult_to_guess'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+
+    # def validate_email(form, field):
+    #     if "utoronto" not in field.data:
+    #         raise ValidationError("Email must be a valid UofT email address!")
+
+    email = StringField('What is your UofT email?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name, current_time=datetime.now(timezone.utc))
+    return render_template('user.html', name=session.get('name'), current_time=datetime.now(timezone.utc))
 
 @app.errorhandler(404)
 def page_not_found(e):
